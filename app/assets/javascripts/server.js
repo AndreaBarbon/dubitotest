50bfaaa872eac115a5be9c26f25aa3e2d578391d
AndreaBarbon <andrea.3arbon@gmail.com>
2012-06-22 14:52:51 +0200

    initial

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://dubitotest.herokuapp.com/faye');
+  var faye = new Faye.Client('http://dubitoserver.herokuapp.com/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
