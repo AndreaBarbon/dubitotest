480324388fcc965e4d5a8584e1801b6473fd974b
AndreaBarbon <andrea.3arbon@gmail.com>
2012-06-22 16:01:44 +0200

    initial

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -1,6 +1,11 @@
 $(function() {
-  var faye = new Faye.Client('http://dubitoserver.herokuapp.com/faye');
-  faye.subscribe("/messages/new", function(data) {
-    eval(data);
+	var faye = new Faye.Client('http://dubitoserver.herokuapp.com/faye');
+  faye.subscribe('/messages/new', function (data) {
+    console.log(data);
+    $("#chat").append('<li>' + data.object.body + '</li>')
+  });
+
+  $("#new_message").live("ajax:complete", function(event,xhr,status){
+    $('#new_message')[0].reset();
   });
 });
