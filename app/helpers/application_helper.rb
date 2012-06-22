db1cf417db423265db6a0616b98a4ba0cfc14acb
AndreaBarbon <andrea.3arbon@gmail.com>
2012-06-22 17:56:48 +0200

    initial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  def broadcast(channel, &block)
-    message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://dubitoserver.herokuapp.com/faye")
-    Net::HTTP.post_form(uri, :message => message.to_json)
-  end
 end
