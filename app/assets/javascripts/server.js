d95d8723118085d2bb2773c86192327e8764124e
AndreaBarbon <andrea.3arbon@gmail.com>
2012-06-22 15:13:53 +0200

    initial

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -1,11 +1,6 @@
 $(function() {
-	var faye = new Faye.Client('http://dubitoserver.herokuapp.com/faye');
-  faye.subscribe('/messages/new', function (data) {
-    console.log(data);
-    $("#chat").append('<li>' + data.object.body + '</li>')
-  });
-
-  $("#new_message").live("ajax:complete", function(event,xhr,status){
-    $('#new_message')[0].reset();
+  var faye = new Faye.Client('http://dubitoserver.herokuapp.com/faye');
+  faye.subscribe("/messages/new", function(data) {
+    eval(data);
   });
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,15 +5,5 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.create!(params[:message])
-    broadcast("/messages/new", @message)
-    render :json => @message
-  end
-
-  private
-
-  def broadcast(channel, object)
-    message = {:channel => channel, :data => { :object => object, :type => "message" } }
-    uri = URI.parse("http://dubitoserver.herokuapp.com/faye")
-    Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
