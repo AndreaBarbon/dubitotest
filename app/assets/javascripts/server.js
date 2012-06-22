19eb06eb6454346add15f24163753e4196f5400b
AndreaBarbon <andrea.3arbon@gmail.com>
2012-06-22 16:07:07 +0200

    initial

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -1,8 +1,8 @@
 $(function() {
 	var faye = new Faye.Client('http://dubitoserver.herokuapp.com/faye');
-  faye.subscribe('/messages/new', function (data) {
+  faye.subscribe('/messages/new', function(data) {
     console.log(data);
-    $("#chat").append('<li>' 'Ciao' '</li>')
+    $("#chat").append('<li>Ciao</li>')
   });
 
   $("#new_message").live("ajax:complete", function(event,xhr,status){
